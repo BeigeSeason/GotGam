f5f9d96c6cb4aadd6bb5fe94586c29f62c61fe75
BeigeSeason <kima606@gmail.com>
2025-03-19 12:00:45 +0900

    2025.03.19 새로고침 시 에러 수정, 스케줄러 설정

--- src/main/java/com/springboot/gotgam/GotGamApplication.java
+++ src/main/java/com/springboot/gotgam/GotGamApplication.java
@@ -2,8 +2,12 @@ package com.springboot.gotgam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class GotGamApplication {
 
     public static void main(String[] args) {
